cb251d36049f15148320156f35b01de9353bc4a6
Marty Pye <marty.pye@rwth-aachen.de>
2013-11-20 18:31:17 +0100

    Created project and added storyboard

---
+++ AlienInvaders/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AlienInvaders
+//
+//  Created by Marty on 11/20/13.
+//  Copyright (c) 2013 Marty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ AlienInvadersTests/AlienInvadersTests.m
@@ -0,0 +1,34 @@
+//
+//  AlienInvadersTests.m
+//  AlienInvadersTests
+//
+//  Created by Marty on 11/20/13.
+//  Copyright (c) 2013 Marty. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AlienInvadersTests : XCTestCase
+
+@end
+
+@implementation AlienInvadersTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
